0ebe0f813c4909dcab54408228f0e0cd86b6f2a6
Weiqi97 <weltch1997@gmail.com>
2024-10-29 12:13:31 -0400

    add documents

--- tests/test_bp.cpp
+++ tests/test_bp.cpp
@@ -2,6 +2,7 @@
 #include "bp.hpp"
 
 TEST(RBPTests, Pairing){
+    // Create the binary pairing group.
     const auto Bp = BP();
 
     const auto x = Bp.Zp->rand_vec(10);
@@ -13,8 +14,7 @@ TEST(RBPTests, Pairing){
     const auto g2x = Bp.Gp->g2_raise(x);
     const auto g2y = Bp.Gp->g2_raise(y);
 
-    const bool r = Group::cmp_gt(Group::pair(g1x, g2y), Group::pair(g1y, g2x));
-
+    // Test and then clear the core.
+    EXPECT_TRUE(Group::cmp_gt(Group::pair(g1x, g2y), Group::pair(g1y, g2x)));
     BP::close();
-    EXPECT_TRUE(r);
 }
